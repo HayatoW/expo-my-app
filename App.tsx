beb218552073dd45cf0b3bf7bad15e8fc613f12c
Hayato Watanabe <solideye@me.com>
2021-04-25 00:37:58 +0900

    Time for a code organization break!

--- App.tsx
+++ App.tsx
@@ -5,9 +5,9 @@ import { Image, StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={{ width: 305, height: 159 }} />
+      <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={styles.logo} />
 
-      <Text style={{color: '#888', fontSize: 18}}>
+      <Text style={styles.instructions}>
         To share a photo from your phone with a friend, just press the button below!
       </Text>
       <StatusBar style="auto" />
@@ -21,5 +21,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  logo: {
+    width: 305,
+    height: 159,
+    marginBottom: 10,
+  },
+  instructions: {
+    color: '#888',
+    fontSize: 18,
+    marginHorizontal: 15,
   },
 });
